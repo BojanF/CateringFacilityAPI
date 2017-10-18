9ca78ff573774865826a34a87687ef6763e39112
BojanF <boko1994@gmail.com>
2017-10-18 12:42:19 +0200

    Setting project structure

---
+++ src/main/java/com/apiApp/CateringFacilityAPI/CateringFacilityApiApplication.java
@@ -0,0 +1,12 @@
+package com.apiApp.CateringFacilityAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringFacilityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CateringFacilityApiApplication.class, args);
+	}
+}
